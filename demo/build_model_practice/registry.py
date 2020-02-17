dd2981b9056ff090c52471273512f5019b81c379
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2020-02-17 18:55:58 +0900

    Update registry.py

--- demo/build_model_practice/registry.py
+++ demo/build_model_practice/registry.py
@@ -1,25 +1,8 @@
 from typing import Dict, Optional
 from detectron2.modeling import build_model
-
-class RG(object):
-        def __init__(self, name: str) -> None:
-                self._name = name
-                self._obj_map : Dict[str, object] = {}
-                        
-        def get(self, name: str) -> object:
-                ret = self._obj_map.get(name)
-                if ret is None:
-                    raise KeyError(
-                        "No object named '{}' found in '{}' registry!".format(
-                            name, "ABCD"
-                        )
-                    )
-                return ret
-
-
-print("Registry")
-print(Registry)
-def build_model(cfg):
+from detectron2.utils.registry import Registry
+META_ARCH_REGISTRY = Registry("META_ARCH")
+def bm_practice(cfg):
     meta_arch = cfg.MODEL.META_ARCHITECTURE
     return META_ARCH_REGISTRY.get(meta_arch)(cfg)
     
