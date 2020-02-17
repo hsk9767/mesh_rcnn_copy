de2e8fd182c365837b8df17c88fa618ebe4376f3
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2020-02-17 17:04:12 +0900

    Update registry.py

--- demo/build_model_practice/registry.py
+++ demo/build_model_practice/registry.py
@@ -1,17 +1,22 @@
-from fvcore.common.registry import Registry
+from typing import Dict, Optional
 
-META_ARCH_REGISTRY = Registry("META_ARCH")  # noqa F401 isort:skip
+class RG(object):
+        def __init__(self, name: str) -> None:
+                self._name = name
+                self._obj_map : Dict[str, object] = {}
+                        
+        def get(self, name: str) -> object:
+                ret = self._obj_map.get(name)
+                if ret is None:
+                    raise KeyError(
+                        "No object named '{}' found in '{}' registry!".format(
+                            name, "ABCD"
+                        )
+                    )
+                return ret
 
-def get(name: str) -> object:
-        ret = self._obj_map.get(name)
-        if ret is None:
-            raise KeyError(
-                "No object named '{}' found in '{}' registry!".format(
-                    name, "ABCD"
-                )
-            )
-        return ret
+META_ARCH_REGISTRY = RG("META_ARCH")
 def bm_practice(cfg):
     meta_arch = cfg.MODEL.META_ARCHITECTURE
-    return get(meta_arch)(cfg)
+    return META_ARCH_REGISTRY.get(meta_arch)(cfg)
     
