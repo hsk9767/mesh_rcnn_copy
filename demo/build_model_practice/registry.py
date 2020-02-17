fc1db95c60f10d8ff554c94d01570821c01bc2e4
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2020-02-17 16:57:03 +0900

    Update registry.py

--- demo/build_model_practice/registry.py
+++ demo/build_model_practice/registry.py
@@ -2,12 +2,12 @@ from fvcore.common.registry import Registry
 
 META_ARCH_REGISTRY = Registry("META_ARCH")  # noqa F401 isort:skip
 
-def get(self, name: str) -> object:
+def get(name: str) -> object:
         ret = self._obj_map.get(name)
         if ret is None:
             raise KeyError(
                 "No object named '{}' found in '{}' registry!".format(
-                    name, self._name
+                    name, "ABCD"
                 )
             )
         return ret
