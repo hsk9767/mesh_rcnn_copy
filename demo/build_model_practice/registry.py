ab1c432b643b82f0733509a0a1395279b6fe7577
hsk9767 <34028332+hsk9767@users.noreply.github.com>
2020-02-18 10:42:08 +0900

    Update and rename registry.py to practice.py

--- demo/build_model_practice/registry.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Optional
-from detectron2.modeling import build_model
-from detectron2.utils.registry import Registry
-META_ARCH_REGISTRY = Registry("META_ARCH")
-def bm_practice(cfg):
-    meta_arch = cfg.MODEL.META_ARCHITECTURE
-    return META_ARCH_REGISTRY.get(meta_arch)(cfg)
-    
